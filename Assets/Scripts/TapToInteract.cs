b35323c4d6d79af7427377adc9f6c27ec1d81eae
arochaix <arochaix@outlook.com>
2018-07-24 16:10:43 -0700

    Added interactive menu & interactive door

--- Assets/Scripts/TapToInteract.cs
+++ Assets/Scripts/TapToInteract.cs
@@ -6,17 +6,29 @@ using HoloToolkit.Unity.InputModule;
 public class TapToInteract : MonoBehaviour, IInputClickHandler
 {
     public bool clicked = false;
-    private Sprite[] Icons;
+    public float rotationspeed = 1.0f;
+    public Sprite[] Icons;
 
     public void Awake()
     {
         //Load all sprites from the menu button folder in Sprites Folder
-        object[] loadedIcons = Resources.LoadAll("Sprites/MenuButton", typeof(Sprite));
+        object[] loadedIcons = Resources.LoadAll("MenuButton", typeof(Sprite));
         Icons = new Sprite[loadedIcons.Length];
         for(int x = 0; x < loadedIcons.Length; ++x)
         {
             Icons[x] = (Sprite)loadedIcons[x];
         }
+
+        //By default set all children items of menu to inactive
+        ToggleChildren(false);
+    }
+
+    public void Update()
+    {
+        //Always have this UI button face the user
+        //this.transform.LookAt(Camera.main.transform.position, Camera.main.transform.up);
+        var neededrotation = Quaternion.LookRotation(Camera.main.transform.position - this.transform.position);
+        Quaternion.Slerp(this.transform.rotation, neededrotation, Time.deltaTime * rotationspeed);
     }
 
     public void OnInputClicked(InputClickedEventData eventData)
@@ -25,11 +37,13 @@ public class TapToInteract : MonoBehaviour, IInputClickHandler
         {
             clicked = true;
             SwitchSprites(0);
+            ToggleChildren(true);
         }
         else if(clicked == true)
         {
             clicked = false;
             SwitchSprites(1);
+            ToggleChildren(false);
         }
     }
 
@@ -39,15 +53,33 @@ public class TapToInteract : MonoBehaviour, IInputClickHandler
         {
             case 0:
                 //switch to up arrows
-                this.gameObject.GetComponent<Button>().image.sprite = Icons[0]; 
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Icons[0]; 
                 break;
             case 1:
                 //switch to down arrows
-                this.gameObject.GetComponent<Button>().image.sprite = Icons[1];
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Icons[1];
                 break;
             default:
                 //throw exception here since impossible case
                 break;
+        }
+    }
+
+    private void ToggleChildren(bool enable)
+    {
+        if(enable == true)
+        {
+            foreach(Transform child in this.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+        else if (enable == false)
+        {
+            foreach (Transform child in this.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
         }
     }
 }
---
+++ Assets/Scripts/TapToOpenDoor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity.InputModule;
+
+public class TapToOpenDoor : MonoBehaviour, IInputClickHandler
+{
+    public bool open = false;
+    public bool active = false;
+    public float speed = 1.0f;
+    public float openAngle = 90.0f;
+    private float angleError = 0.5f;
+    private Quaternion startRotation;
+    private Quaternion endRotation;
+    private Vector3 zero;
+
+    public void Awake()
+    {
+        //if inactive & closed, save the values of startrotation & endrotation
+        if (active == false && open == false)
+        {
+            zero = this.transform.forward;
+            startRotation = this.transform.rotation;
+            this.transform.RotateAround(transform.position, transform.up, openAngle);
+            endRotation = this.transform.rotation;
+            this.transform.rotation = startRotation;
+        }
+    }
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        
+        //User wants to interact with door
+        if(open == false)
+        {
+            open = true;
+        }
+        else if(open == true)
+        {
+            open = false;
+        }
+
+        //Action is being performed
+        active = true;
+    }
+
+    public void Update()
+    {
+        //if user interacted with the door
+        if(active == true)
+        {
+            //open the door
+            if(open == true)
+            {
+                OpenDoor();
+            }
+            //close the door
+            else if(open == false)
+            {
+                CloseDoor();
+            }
+        }
+    }
+
+    private float previousAngle = 0.0f;
+    private bool valid = false;
+    private bool closeDoor = true;
+    private void CloseDoor()
+    {
+        var forward = this.gameObject.transform.forward; 
+        float rotationAngle = Vector3.Angle(zero, forward);
+        float difference = rotationAngle - previousAngle;
+        if (valid)
+        {
+            if(difference > 0.0f)
+            {
+                closeDoor = false;
+            }
+        }
+        previousAngle = rotationAngle;
+        valid = true;
+        if (closeDoor)
+        {
+            transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed * -1.0f * openAngle);
+        }
+        else {
+            this.transform.rotation = startRotation;
+            active = false;
+            valid = false;
+            closeDoor = true;
+        }
+    }
+
+    private void OpenDoor()
+    {
+        var forward = this.gameObject.transform.forward;
+        float rotationAngle = Vector3.Angle(zero, forward);
+        if (rotationAngle < openAngle)
+        {
+            transform.RotateAround(transform.position, transform.up, Time.deltaTime * speed * openAngle);
+        }
+        else
+        {
+            this.transform.rotation = endRotation;
+            active = false;
+        }
+    }
+}
